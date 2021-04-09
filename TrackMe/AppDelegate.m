3c2f2c3b756ae32012739ed43125c11c74e5b769
Hanz Meyer <hanzcmeyer@gmail.com>
2021-04-09 12:30:59 -0400

    Updated AppDelegate and GLManager classes.

--- TrackMe/AppDelegate.m
+++ TrackMe/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "GLManager.h"
 
 @interface AppDelegate ()
 
@@ -13,28 +14,72 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSLog(@"Application launched with options: %@", launchOptions);
+    
+    [GLManager sharedManager];
+    
+    if([launchOptions objectForKey:UIApplicationLaunchOptionsLocationKey]) {
+        [[GLManager sharedManager] logAction:@"application_launched_with_location"];
+    }
+    
+    return YES;
 }
 
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [[GLManager sharedManager] applicationWillResignActive];
 }
 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"Application is entering the background");
+    [[GLManager sharedManager] applicationDidEnterBackground];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"Application is terminating");
+    [[GLManager sharedManager] applicationWillTerminate];
+}
+
+// App launched by clicking a URL
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    if([[url host] isEqualToString:@"setup"]) {
+        NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:NO];
+        NSArray *queryItems  = urlComponents.queryItems;
+        NSString *endpoint = [self queryValueForKey:@"url" fromQueryItems:queryItems];
+        NSString *token    = [self queryValueForKey:@"token" fromQueryItems:queryItems];
+        NSString *deviceId = [self queryValueForKey:@"device_id" fromQueryItems:queryItems];
+        NSLog(@"Saving new config endpoint=%@ token=%@ device_id=%@", endpoint, token, deviceId);
+        [[GLManager sharedManager] saveNewDeviceId:deviceId];
+        [[GLManager sharedManager] saveNewAPIEndpoint:endpoint andAccessToken:token];
+    }
+    
+    return YES;
+}
+
+- (NSString *)queryValueForKey:(NSString *)key fromQueryItems:(NSArray *)queryItems
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name=%@", key];
+    NSURLQueryItem *queryItem = [[queryItems filteredArrayUsingPredicate:predicate] firstObject];
+    return queryItem.value;
+}
 
 @end
